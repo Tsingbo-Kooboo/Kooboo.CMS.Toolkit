627ca4fb3e9f6d7c769914ea7d8a8db06618ae7b
Tsingbo-Kooboo <qingbo@kooboo.com>
2016-12-01 23:01:40 +0800

    UEditor

---
+++ Kooboo.CMS.Toolkit.Controls.RichTextEditors/Controllers/UEditorController.cs
@@ -0,0 +1,19 @@
+﻿using Kooboo.CMS.Web.Authorizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Kooboo.CMS.Web.Areas.Contents.Controllers
+{
+    [LargeFileAuthorization(AreaName = "Contents", Group = "", Name = "MediaLibrary", Order = 1)]
+    public class UEditorController : ContentControllerBase
+    {
+        public ActionResult Browser(string action)
+        {
+            return Content("Hello");
+        }
+    }
+}
