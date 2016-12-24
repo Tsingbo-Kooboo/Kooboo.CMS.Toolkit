d3bf8cb56fe762135d1f64eb120961b8efc5f4f6
Tsingbo-Kooboo <qingbo@kooboo.com>
2016-12-24 22:51:25 +0800

    extensions

--- Kooboo.CMS.Toolkit.Controls.RichTextEditors/UEditor.cs
+++ Kooboo.CMS.Toolkit.Controls.RichTextEditors/UEditor.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kooboo.CMS.Form;
+using Kooboo.Web.Script.Serialization;
+using System.Web;
 
 namespace Kooboo.CMS.Toolkit.Controls.RichTextEditors
 {
@@ -14,10 +16,11 @@ namespace Kooboo.CMS.Toolkit.Controls.RichTextEditors
 
         protected override string RenderInput(IColumn column)
         {
+            var columnSettings = column.CustomSettings ?? new Dictionary<string, string>();
+            var settingArray = columnSettings.Select(it => $"{it.Key}:{it.Value}");
             return $@"<div class=""extra-large""><script name=""{column.Name}"" id=""{column.Name}"" class=""{0} ueditor"" 
-media_library_url=""@Url.Action(""Selection"",""MediaContent"",ViewContext.RequestContext.AllRouteValues()))"" 
-media_library_title =""@(""Selected Files"".Localize())"" type=""text/plain"">@Html.Raw(Model.{column.Name} ?? """")</script></div>
-<script>UE.getEditor(""{column.Name}"").addListener(""contentChange"", function () {{$.publish(kooboo.constants.messageToptics.SomeThing_Changed_On_Page);}});</script>
+ type=""text/plain"">@Html.Raw(Model.{column.Name} ?? """")</script></div>
+<script>UE.getEditor(""{column.Name}"",{{{string.Join(",", settingArray.ToArray()).Replace(" ", "")}}}).addListener(""contentChange"", function () {{$.publish(kooboo.constants.messageToptics.SomeThing_Changed_On_Page);}});</script>
 ";
         }
     }
